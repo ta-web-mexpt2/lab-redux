df71f13ab2dc5da6eb61e69b985935a44ac24ada
carlosgvinuesa <56604586+carlosgvinuesa@users.noreply.github.com>
2020-06-16 18:28:37 -0500

    ahi vamos

--- starter-code/src/index.js
+++ starter-code/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from "react-redux"
+import store from "./redux/store"
+
+const WithProvider = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WithProvider />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ starter-code/src/redux/store.js
@@ -0,0 +1,17 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import counter from "./reducer";
+import todos from "./TodosDuck";
+
+const rootReducer = combineReducers({
+  counter,
+  todos,
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+export default store;
